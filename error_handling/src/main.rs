3cac93d13d099c3814afd8478523e20af5241596
cameroncarlg <cameroncarlg@gmail.com>
2025-10-19 19:14:54 -0700

    more error handling

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,24 +1,45 @@
-use std::{fs::File, io::ErrorKind};
+use std::{
+    fs,
+    //fs::File,
+    //io::{Error, ErrorKind, Read},
+    io::Error,
+};
+
+fn read_username_from_file() -> Result<String, Error> {
+    // if the value from Result is Ok -> place it in to variable
+    // "?" allows us to have syntactic sugar over Result<T,E>
+
+    //let mut username = String::new();
+
+    //File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    //ok(username)
+
+    // even shorter...
+    fs::read_to_string("hello.txt")
+}
 
 fn main() {
-    let greeting_file_result = File::open("hello.txt");
+    let username = read_username_from_file();
+    println!("{}", username.unwrap().trim());
+    //println!("{}", username.unwrap().len());
 
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            // match on the error. If the file is not found,
-            // create it (fc)
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {e:?}"),
-            },
+    //let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+    //    if error.kind() == ErrorKind::NotFound {
 
-            // anything else return this
-            _ => {
-                panic!("Problem opening the file: {error:?}");
-            }
-        },
-    };
+    //        // 'unwrap_or_else' just mean execute the create file method
+    //        // unless theres an error, if an error, panic
+    //        File::create("hello.txt").unwrap_or_else(|error| {
+    //            panic!("Problem creating the file: {error:?}");
+    //        })
+    //    } else {
+    //        panic!("Problem opening the file: {error:?}");
+    //    }
+    //});
 
-    println!("{:?}", greeting_file);
+    // here we can spit out text for the error
+    //let greeting_file =
+    //    File::open("hello.txt").expect("hello.txt should be included in this project");
+
+    //println!("{:?}", greeting_file);
 }
