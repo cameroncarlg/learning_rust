6194862967791af8836c8a0ce2a007e26e8a1070
cameroncarlg <cameroncarlg@gmail.com>
2025-10-23 16:09:28 -0700

    finished generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -20,15 +20,26 @@ fn find_largest_char(v: &Vec<char>) -> &char {
     current_largest
 }
 
-fn find_largest<T>(v: &[T]) -> &T {
-    let mut current_largest = &v[0];
+//fn find_largest<T>(v: &[T]) -> &T {
+//    let mut current_largest = &v[0];
+//
+//    for i in v {
+//        if i > current_largest {
+//            current_largest = i;
+//        }
+//    }
+//    current_largest
+//}
 
-    for i in v {
-        if i > current_largest {
-            current_largest = i;
-        }
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
     }
-    current_largest
 }
 
 fn main() {
@@ -38,6 +49,17 @@ fn main() {
     let largest_int = find_largest_num(&my_vec);
     let largest_char = find_largest_char(&char_vec);
 
+    let point1 = Point { x: 3, y: 4 };
+    let point2 = Point { x: 3.5, y: 4.0 };
+    let point3 = Point { x: 3.8, y: 4 };
+
+    //let largest_gen1 = find_largest(&my_vec);
+
     println!("{largest_int}");
     println!("{largest_char}");
+
+    println!("{}", point1.x());
+    println!("{}", point2.x());
+    println!("{}", point3.x());
+    println!("{}", point3.y);
 }
